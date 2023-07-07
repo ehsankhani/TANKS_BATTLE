a84421a01be5c85d34cb2a3a58f34fd036dbf80d
ehsan <ehsanhamzekhani1380@gmail.com>
2023-07-08 00:57:23 +0330

    Add TankInfo GroupBox To PlayerInfoUI

--- playerinfoui.cpp
+++ playerinfoui.cpp
@@ -18,3 +18,73 @@ PlayerInfoUI::~PlayerInfoUI()
 {
     delete ui;
 }
+
+void PlayerInfoUI::on_CmbTankPlayerOne_currentIndexChanged(const QString &arg1)
+{
+    for(int i = 0; i < this->Tanks.length(); i++)
+    {
+        if(this->Tanks[i].GetName() == arg1)
+        {
+            ui->LblTankNameOne->setText("نام : " + this->Tanks[i].GetName());
+            ui->LblTankSpeedOne->setText("سرعت : " + QString::number(this->Tanks[i].GetSpeed()));
+            ui->LblTankPowerOne->setText("قدرت : " + QString::number(this->Tanks[i].GetPower()));
+            ui->LblTankShildOne->setText("تحمل زره : " + QString::number(this->Tanks[i].GetShild()));
+            QString result = "رنگ : ";
+            switch (this->Tanks[i].GetColor())
+            {
+            case 1:
+                result += "قرمز";
+                break;
+            case 2:
+                result += "سبز";
+                break;
+            case 3:
+                result += "زرد";
+                break;
+            case 4:
+                result += "سیاه";
+                break;
+            case 5:
+                result += "آبی";
+                break;
+            }
+            ui->LblTankColorOne->setText(result);
+            break;
+        }
+    }
+}
+
+void PlayerInfoUI::on_CmbTankPlayerTwo_currentIndexChanged(const QString &arg1)
+{
+    for(int i = 0; i < this->Tanks.length(); i++)
+    {
+        if(this->Tanks[i].GetName() == arg1)
+        {
+            ui->LblTankNameTwo->setText("نام : " + this->Tanks[i].GetName());
+            ui->LblTankSpeedTwo->setText("سرعت : " + QString::number(this->Tanks[i].GetSpeed()));
+            ui->LblTankPowerTwo->setText("قدرت : " + QString::number(this->Tanks[i].GetPower()));
+            ui->LblTankShildTwo->setText("تحمل زره : " + QString::number(this->Tanks[i].GetShild()));
+            QString result = "رنگ : ";
+            switch (this->Tanks[i].GetColor())
+            {
+            case 1:
+                result += "قرمز";
+                break;
+            case 2:
+                result += "سبز";
+                break;
+            case 3:
+                result += "زرد";
+                break;
+            case 4:
+                result += "سیاه";
+                break;
+            case 5:
+                result += "آبی";
+                break;
+            }
+            ui->LblTankColorTwo->setText(result);
+            break;
+        }
+    }
+}
